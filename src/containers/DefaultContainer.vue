6a4e08a7e5fe26f0509b93b1fc7fd9dba582d87c
urodriguez <urodriguez@velocitypartners.net>
2018-09-03 08:33:14 -0300

    clean header

--- src/containers/DefaultContainer.vue
+++ src/containers/DefaultContainer.vue
@@ -8,9 +8,9 @@
       </b-link>
       <SidebarToggler class="d-md-down-none" display="lg" />
       <b-navbar-nav class="d-md-down-none">
-        <b-nav-item class="px-3" to="/dashboard">Dashboard</b-nav-item>
+<!--         <b-nav-item class="px-3" to="/dashboard">Dashboard</b-nav-item>
         <b-nav-item class="px-3" to="/users" exact>Users</b-nav-item>
-        <b-nav-item class="px-3">Settings</b-nav-item>
+        <b-nav-item class="px-3">Settings</b-nav-item> -->
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-item class="d-md-down-none">
